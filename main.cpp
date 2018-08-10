ff0350be4da8acda062dd356f201974896fb4caf
nathaliamoreno <41310257+nathaliamoreno@users.noreply.github.com>
2018-08-09 22:12:27 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,21 +5,13 @@ using namespace std;
 
 int main()
 {
-    Data data1 = Data(31, 12, 2000);
-   // Data* data2 = new Data (1, 1, 2000);
+    Data data1 = Data(32, 12, 2000);
 
 
-/*    data1.set_dia();
-    data1.get_dia(dia);
-    data1.set_mes();
-    data1.get_mes(mes);
-    data1.set_ano();
-    data1.get_ano(ano);
-*/
+
+    cout << "Data inicial: " << data1.dia << "/" << data1.mes << "/" << data1.ano <<endl;
     data1.AvancarDia();
-
-    cout << data1.dia << "/" << data1.mes << "/" << data1.ano <<endl;
-
+    cout << "Proxima data: "<< data1.dia << "/" << data1.mes << "/" << data1.ano <<endl;
 
 
     return 0;
