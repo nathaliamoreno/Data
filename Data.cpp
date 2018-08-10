8374677d940d5ae39a961770e974da29567dcbe7
nathaliamoreno <41310257+nathaliamoreno@users.noreply.github.com>
2018-08-09 22:17:18 -0300

    Update Data.cpp

--- Data.cpp
+++ Data.cpp
@@ -1,33 +1,32 @@
-#include "Data.h"
+#include "DATA.h"
 #include <iostream>
 
-Data::Data(int d, int m, int a){
+Data::Data(int dia, int mes, int ano){
 
-    dia = d;
-    mes = m;
-    ano = a;
+    this->dia = dia;
+    this->mes = mes;
+    this->ano = ano;
 }
-/*void Data::set_dia(int dia){
-    this.dia = dia;
+void Data::setDia(int dia){
+    this->dia = dia;
  }
-void Data::set_mes(int mes){
-    this.mes = mes;
+void Data::setMes(int mes){
+    this->mes = mes;
  }
-void Data::set_ano(int ano){
-    this.ano = ano;
+void Data::setAno(int ano){
+    this->ano = ano;
  }
-int Data::get_dia(){
+int Data::getDia(){
     return dia;
 }
-int Data::get_mes(){
+int Data::getMes(){
     return mes;
 }
-int Data::get_ano(){
+int Data::getAno(){
     return ano;
- }*/
+ }
 int Data::AvancarDia(){
 
-    dia++;
     if ( mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes || 12 ){
         if (dia == 32){
             dia = 1;
@@ -48,9 +47,5 @@ int Data::AvancarDia(){
     if (mes == 13){
        mes = 1;
        ano++;
-
-     /*}else{
-       mes++;
-     }*/
-
-}}
+    }
+}
